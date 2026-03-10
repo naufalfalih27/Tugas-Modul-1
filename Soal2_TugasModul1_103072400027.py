f4ae73fb997617b8d109ddd3ef7b000329fc3ece
naufalfalih27 <falih6090z@gmail.com>
2026-03-10 12:23:11 +0700

    update

--- Soal2_TugasModul1_103072400027.py
+++ Soal2_TugasModul1_103072400027.py
@@ -1,15 +1,18 @@
+#mencari satu angka Fibonacci pada posisi ke-n
 def fibonacci_ke(n):
     a, b = 0, 1
     for i in range(n):
+        #update nilai: a jadi b, dan b jadi hasil penjumlahan a + b    
         a, b = b, a + b
     return a
 
+#mencetak seluruh barisan Fibonacci sampai suku ke-n
 def Barisan_Fibonacci(n):
     a, b = 0, 1
-    barisan = []
+    barisan = [] #list kosong untuk menampung angka
     for i in range(n):
         a, b = b, a + b
-        barisan.append(a)
+        barisan.append(a) #memasukkan angka hasil penjumlahan ke dalam list
     print("Barisannya:", barisan)
 
 n = int(input("Test sampai suku ke berapa ? "))
