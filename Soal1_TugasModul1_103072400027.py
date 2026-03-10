e9ebd8ebe216990b1aaa3864ac61c5a92926ef6c
naufalfalih27 <falih6090z@gmail.com>
2026-03-10 12:19:05 +0700

    update

--- Soal1_TugasModul1_103072400027.py
+++ Soal1_TugasModul1_103072400027.py
@@ -1,8 +1,9 @@
 def isKabisat(tahun):
+    #Syarat kabisat: habis dibagi 4 DAN tidak habis dibagi 100, ATAU habis dibagi 400
     if (tahun % 4 == 0 and tahun % 100 != 0) or (tahun % 400 == 0):
-        return True
+        return True # Mengembalikan nilai True jika syarat terpenuhi
     else:
-        return False
+        return False # Mengembalikan nilai False jika syarat tidak terpenuhi
 
 tahun_input = int(input("Masukkan tahun: "))
 
